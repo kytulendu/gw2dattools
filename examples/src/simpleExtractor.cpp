d1211d4239fd2e6690a7d291c75468e52380de05
Khral Steelforge <garuda2550@gmail.com>
2016-01-07 23:02:47 +0700

    Make use of range based loop in sinpleExtractor.
    Signed-off-by: Khral Steelforge <garuda2550@gmail.com>

--- examples/src/simpleExtractor.cpp
+++ examples/src/simpleExtractor.cpp
@@ -8,57 +8,52 @@
 #include "gw2DatTools/compression/inflateDatFileBuffer.h"
 
 int main( int argc, char* argv[] ) {
-	const uint32_t aBufferSize = 1024 * 1024 * 30; // We make the assumption that no file is bigger than 30 M
+	const uint32_t aBufferSize = 1024 * 1024 * 30; // We make the assumption that no file is bigger than 30 Mb
 
 	auto datfile = "C:\\Games\\Guild Wars 2\\Gw2.dat";
 
 	auto pANDatInterface = gw2dt::datfile::createANDatInterface( datfile );
-
-	if ( !pANDatInterface ) {
-		std::cout << "Unable to load " << datfile << std::endl;
-		exit( EXIT_FAILURE );
-	}
-
 	auto aFileRecordVect = pANDatInterface->getFileRecordVect( );
 
 	uint8_t* pOriBuffer = new uint8_t[aBufferSize];
 	uint8_t* pInfBuffer = new uint8_t[aBufferSize];
 
-	for ( auto it = aFileRecordVect.begin( ); it != aFileRecordVect.end( ); ++it ) {
+	for ( auto& it : aFileRecordVect ) {
 		uint32_t aOriSize = aBufferSize;
-		pANDatInterface->getBuffer( *it, aOriSize, pOriBuffer );
+		pANDatInterface->getBuffer( it, aOriSize, pOriBuffer );
 
-		std::cout << "Processing File " << it->fileId << std::endl;
+		std::cout << "Processing File " << it.fileId << std::endl;
 
-		//std::ofstream aOFStream;
-		//std::ostringstream oss;
-		//oss << "D:\\gw2Unpack\\" << it->fileId;
-		//aOFStream.open(oss.str().c_str(), std::ios::binary | std::ios::out);
+		std::ofstream aOFStream;
+		std::ostringstream oss;
+		oss << "Z:\\" << it.fileId;
+		aOFStream.open( oss.str( ).c_str( ), std::ios::binary | std::ios::out );
 
 		if ( aOriSize == aBufferSize ) {
-			std::cout << "File " << it->fileId << " has a size greater than (or equal to) 30Mo." << std::endl;
+			std::cout << "File " << it.fileId << " has a size greater than (or equal to) 30Mb." << std::endl;
 		}
 
-		if ( it->isCompressed ) {
-			std::cout << "Compressed file." << std::endl;
-
+		if ( it.isCompressed ) {
 			uint32_t aInfSize = aBufferSize;
 
 			try {
 				gw2dt::compression::inflateDatFileBuffer( aOriSize, pOriBuffer, aInfSize, pInfBuffer );
-				//aOFStream.write(reinterpret_cast<const char*>(pInfBuffer), aInfSize);
+				aOFStream.write( reinterpret_cast<const char*>( pInfBuffer ), aInfSize );
 			} catch ( std::exception& iException ) {
-				std::cout << "File " << it->fileId << " failed to decompress: " << std::string( iException.what( ) ) << std::endl;
+				std::cout << "File " << it.fileId << " failed to decompress: " << std::string( iException.what( ) ) << std::endl;
 			}
 		} else {
+
+			// skip 4 byte every 65532 byte here
+
 			//aOFStream.write(reinterpret_cast<const char*>(pOriBuffer), aOriSize);
 		}
 
-		//aOFStream.close();
+		aOFStream.close( );
 	}
 
 	delete[] pOriBuffer;
 	delete[] pInfBuffer;
 
-	exit( EXIT_SUCCESS );
+	return 0;
 };
